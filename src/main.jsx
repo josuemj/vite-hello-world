283ab2e7a871418849f972b3da4ba24725913ae8
josuemj <josuemj456gmail.com>
2024-03-14 22:13:59 -0700

    changes added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Name from './Name.jsx'
+import './Name.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Name name="Josue" />
   </React.StrictMode>,
 )
